0d1af994fe31d8901a0c97d96c308ad391137efe
KalyaniDeva <136910885+KalyaniDeva@users.noreply.github.com>
2023-06-17 20:18:34 +0530

    Add files via upload

---
+++ week1/day2/Mobile.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String bodyColor="Blue";
+		String brand="ViVo";
+		float storage=89.234f;
+		boolean isCharged=false;
+		long imeNumber=11100346799657l;
+		String name="Y83";
+		char logo='V';
+		System.out.println(name);
+		System.out.println(bodyColor);
+		System.out.println(logo);
+		System.out.println(brand);
+		System.out.println(storage);
+		System.out.println(isCharged);
+		System.out.println(imeNumber);
+				
+	}
+
+}

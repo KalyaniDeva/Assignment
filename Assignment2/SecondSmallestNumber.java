c57f19008aede98b6d2f9d66726ed8562a8b160f
KalyaniDeva <136910885+KalyaniDeva@users.noreply.github.com>
2023-07-18 12:36:34 +0530

    Add files via upload

---
+++ Assignment2/SecondSmallestNumber.java
@@ -0,0 +1,17 @@
+package week1.day2.Assignment2;
+
+import java.util.Arrays;
+
+public class SecondSmallestNumber {
+
+	public static void main(String[] args) {
+
+		int sum[]={23,45,67,32,89,22}; 		// intialize the array
+		System.out.println("Before sort : "+sum[1]);// before sort print array
+		Arrays.sort(sum); 					// sort the array , 22,23,32,45,67
+		
+		System.out.println("After sort : "+sum[1]); // before sort print array
+		
+	}
+
+}

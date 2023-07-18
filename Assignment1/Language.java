fb731a691d99561e2071c7d21dceae9561c6ca3a
KalyaniDeva <136910885+KalyaniDeva@users.noreply.github.com>
2023-07-18 12:50:33 +0530

    Delete Assignment1 directory

--- Assignment1/Language.java
+++
@@ -1,7 +0,0 @@
-package week3.day1.Assignment1;
-
-public interface Language {
-	
-	public void java();
-
-}
--- Assignment1/TestTool.java
+++
@@ -1,8 +0,0 @@
-package week3.day1.Assignment1;
-
-public interface TestTool extends Language{
-	
-	
-	public void selenium();
-
-}

590e27bc4823627b5bac9433fcbff6f8d3567519
KalyaniDeva <136910885+KalyaniDeva@users.noreply.github.com>
2023-07-18 16:01:26 +0530

    Add files via upload

---
+++ week1day1Assignment1/Browser.java
@@ -0,0 +1,18 @@
+package week1day1Assignment1;
+
+public class Browser {//a) Create a class by name: Browser 
+	
+	public static void main(String[] args) { // Create a main method using shortcut - main + ctrl+ spacebar
+		System.out.println("This is my Browser"); //print a statement --> "This is my Browser"
+
+	}
+
+}
+
+/*
+ * Week1.day1 Assignment Assignment 1: Goal: To understand the class, main method and you sysout  
+ * a) Create a class by name: Browser 
+ * b) Create a main method using shortcut 
+ * c) print a statement --> "This is my Browser" 
+ * d) run and confirm the printed in console
+ */
